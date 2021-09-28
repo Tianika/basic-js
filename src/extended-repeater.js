904fa1d25c4c99659af0cb2a441b8979e3435077
Tianika <tatyriana@yandex.ru>
2021-09-28 14:35:19 +0300

    feat: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,5 +16,44 @@ import { NotImplementedError } from '../extensions/index.js'
  *
  */
 export default function repeater(str, options) {
-  // npm run test ./ test/dream-team.test.js
+  // npm run test ./ test/extended-repeater.test.js
+  let add = options
+
+  add.str = String(add.str)
+  add.separator = String(add.separator)
+  add.addition = String(add.addition)
+  add.additionSeparator = String(add.additionSeparator)
+
+  function addSeparator(str, repeat, separator) {
+    let result = ''
+
+    if (str === 'undefined') {
+      return ''
+    }
+    if (repeat === 'undefined') {
+      return str
+    }
+
+    let temp = new Array(repeat)
+    temp = temp.fill(str, 0)
+    result = temp.join(`${separator}`)
+
+    return result
+  }
+
+  let result = ''
+  let str1 =
+    str +
+    addSeparator(
+      add.addition,
+      add.additionRepeatTimes,
+      add.additionSeparator === 'undefined' ? '|' : add.additionSeparator
+    )
+
+  result = addSeparator(
+    str1,
+    add.repeatTimes,
+    add.separator === 'undefined' ? '+' : add.separator
+  )
+  return result
 }
